99bd4361ad5d80adf7a7fe3a5332259062e965b9
shpatberisha <shpatberisha2510@gmail.com>
2025-06-10 16:03:04 +0200

    gg

--- function.php
+++ function.php
@@ -7,6 +7,8 @@ function load_scripts(){
         filemtime(get_template_directory().'style.css'),'all'
     );
 
+    wp_enqueue_script('dropdown',get_template_directory(),'/js/dropdown.js',array(),'1.0,',false);
+
 
  
 
@@ -14,6 +16,38 @@ function load_scripts(){
 
 
 add_action('wp_enqueue_scripts','load_scripts');
+
+function config() {
+    register_nav_menus(
+        array(
+            'wp_devs_main_menu'=>'Main Menu'
+              'wp_devs_footer_menu'=>'Footer Menu'
+        )
+        );
+
+        $ard=array(
+            'height'=>225,
+            'width'=>1920
+        );
+
+        add_theme_support('custom-header',$args);
+        add_theme_support('post_thumbnails',);
+        add_theme_support('custom-logo',array(
+            'width'=>200,
+            'height'=>110,
+            'flex-height'=>true,
+            'flex-width'=>
+         ));
+         add_theme_support('automatic-feed-links');
+         add_theme_support('html5',array('comment-list','coment-form','search-form','gallery','caption','style','script'));
+         add_theme_support('title-tag');
+
+
+        }
+
+        add_action('after_setup_theme','config',0);
+
+
 
 
 
