6d1792d9e9dda08fa3a9c3b7ca26d817082cbeb6
shpatberisha <shpatberisha2510@gmail.com>
2025-06-26 06:47:54 -0700

    sh

--- parts/content-latest-news.php
+++ parts/content-latest-news.php
@@ -2,19 +2,20 @@
     <?php if(has_post_thumbnail()):?>
         <a href="<?php the_permalink();?>"><?php the_post_thumbnail('large');?></a>
         <?php endif;?>
-        <h3><a href="<?php the_permalink();?>"><?php the_title();?></a><h3>
-            <div class="meta-info">
-                <p>
-                 <?php if(has_category()):?>
+        <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+        <div class="meta-info">
+            <p>
+                by <span><?php the_author_posts_link();?></span>
+                <?php if(has_category()):?>
                 <p>Categories: <?php the_category(' ');?></p>
                 <?php endif;?>
                 <?php if(has_tag()):?>
                     <p>Tags: <?php the_tags('',', ');?></p>
-                <?php endif;?>
+                <?php endif;?> 
+            </p>
+            <p><span><?php echo get_the_date();?></p>
         </div>
-        <?php endif;?>
-                </p>
-                <div class="container">
-       
+        <?php the_excerpt();?>
+
 
 </article>
